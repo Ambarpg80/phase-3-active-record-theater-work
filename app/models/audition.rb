2f098c900817c92027f96df5a65015e76a614df2
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-04-22 17:07:02 -0400

    commit tree 0d96a61cf995820646b7e2a0125e9a88e765ef00

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -1,3 +1,14 @@
 class Audition < ActiveRecord::Base
-  
+  belongs_to :role
+
+
+  def role
+    Role.find_by(:character_name)
+  end
+
+  def callback
+    call = self.find_by(:hired)
+    !call
+  end
+
 end
